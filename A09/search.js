7684c0ac3a8871e17fe37ddbdcb687d42e1f507c
NikLaz101 <68820240+NikLaz101@users.noreply.github.com>
2021-11-22 03:39:52 +0700

    Update search.js

--- A09/search.js
+++ A09/search.js
@@ -1,8 +1,7 @@
-// import product from "./uniqloProduct.js";
-
 const search = document.querySelector("#search");
 const btnSearch = document.querySelector("#searchBar")
-const searchProduct = document.querySelectorAll("#product");
+const divProduct = document.getElementById("products");
+const findProduct = divProduct.getElementsByClassName("product");
 
 btnSearch.style.visibility = "hidden";
 
@@ -12,49 +11,14 @@ search.addEventListener("click", () => {
         document.querySelector("#searchBar").innerHTML += `<input type="text" style="margin:10px;" class="form-control mb-2" placeholder="Search..." id="form">`;
         let searchBox = document.getElementById("form");
         searchBox.addEventListener("keyup", () => {
-            // let input = document.getElementById("form");
-            // let text = input.target.value.toLowerCase().trim();
-            // for (let i = 0; i < searchProduct.length; i++) {
-            //     let txtValue = searchProduct[i].getAttribute();
-            //     if (txtValue.toLowerCase().trim.includes(text)) {
-            //         searchProduct[i].style.display = "";
-            //     } else {
-            //         searchProduct[i].style.display = "none";
-            //     }
-            // }
-            // let filterName = product.filter((allProduct) => {
-            //     return (allProduct.productName.toLowerCase().trim.includes(text))
-            // });
-            // console.log(filterName);
-
-            // let divProduct = searchProduct.children;
-
-            // if (filterName.length == 0) {
-            //     for (const i of divProduct) {
-            //         i.style.display = "none";
-            //     }
-            // } else {
-            //     for (const i of divProduct) {
-            //         for (const j of filterProductByName) {
-            //             if (i.id == j.productId) {
-            //                 i.style.display = "flex";
-            //                 break;
-            //             } else {
-            //                 i.style.display = "none";
-            //             }
-            //         }
-            //     }
-            // }
             let input = document.getElementById("form");
-            let filter = input.value.toLowerCase();
-            let divID = document.getElementById("products");
-            let divClass = divID.getElementsByClassName("product");
-            for (let i = 0; i < divClass.length; i++) {
-                let txtValue = divClass[i].textContent;
-                if (txtValue.toLowerCase().indexOf(filter) > -1) {
-                    divClass[i].style.display = "";
+            let filter = input.value.toLowerCase().trim();
+            for (let i = 0; i < findProduct.length; i++) {
+                let searchProduct = findProduct[i].textContent;
+                if (searchProduct.toLowerCase().trim().includes(filter)) {
+                    findProduct[i].style.display = "";
                 } else {
-                    divClass[i].style.display = "none";
+                    findProduct[i].style.display = "none";
                 }
             }
 
