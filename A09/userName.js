2010a933fb25de6eef73753348c5a9e7f61a215b
NikLaz101 <68820240+NikLaz101@users.noreply.github.com>
2021-11-22 22:41:41 +0700

    Update userName.js

--- A09/userName.js
+++ A09/userName.js
@@ -1,11 +1,11 @@
 import { CookieUtil } from "./cookie.js"
 
-function userNameCookie() {
-    if (CookieUtil.get('userName') == null) {
-        let userCookie = window.prompt("Enter your name:");
-        CookieUtil.set('userName', userCookie, new Date('January 1, 5000'));
+function userNameCookie() { //ฟังก์ชันเก็บ user name
+    if (CookieUtil.get('userName') == null) { //ถ้ายังไม่มี user name ก็จะให้กรอกเข้ามา
+        let userCookie = window.prompt("Enter your name:"); //เก็บ user name ในตัวแปร userCookie
+        CookieUtil.set('userName', userCookie, new Date('January 1, 5000')); //จัดเก็บตัว Cookie ตามชื่อ user และระยะเวลาที่กำหนด
     } else {
-        alert(`Welcome back ${CookieUtil.get('userName')}`);
+        alert(`Welcome back ${CookieUtil.get('userName')}`); //ถ้ามีผู้แล้วก็จะแสดงชื่อผู้ใช้
     }
 }
 
