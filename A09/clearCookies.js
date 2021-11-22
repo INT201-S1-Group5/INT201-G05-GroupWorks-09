8481b1e8e14846f4a966c42b10eb869faed6340a
NikLaz101 <68820240+NikLaz101@users.noreply.github.com>
2021-11-22 22:38:01 +0700

    Update clearCookies.js

--- A09/clearCookies.js
+++ A09/clearCookies.js
@@ -2,11 +2,13 @@ import { CookieUtil } from "./cookie.js"
 
 let clearProducts = document.querySelector('#clearBtn')
 
-function deleteAllCookies() { //ลบตัว cookie ออก โดยตั้งค่าให้อายุของ cookie น้อยกว่าเวลาปัจจุบัน
-    clearProducts.cookie = "total= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-    clearProducts.cookie = "productID= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+function deleteAllCookies() { //ลบตัว cookie ออก 
+    let clearProduct = [];
+    CookieUtil.set('total', clearProduct, new Date(0));
+    CookieUtil.set('productID', clearProduct, new Date(0));
     alert('Clear cart!');
-    let productTotal = [];
+
+    let productTotal = []; //ให้ค่า total เซ็ตเป็น 0 
     let setTotal = document.querySelector('#incart')
     setTotal.textContent = productTotal.length
     CookieUtil.set('total', productTotal.length);
