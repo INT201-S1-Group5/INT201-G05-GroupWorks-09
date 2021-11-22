3d720b7cebe74e5953d7b499f0d0bd846320f287
NikLaz101 <68820240+NikLaz101@users.noreply.github.com>
2021-11-22 23:47:59 +0700

    Update createTag.js

--- A09/createTag.js
+++ A09/createTag.js
@@ -4,60 +4,60 @@ import product from "./uniqloProduct.js";
 const navbarEle = document.querySelector('#navBar');
 
 // รูป search
-let searchBtn = document.createElement('button');
+const searchBtn = document.createElement('button');
 searchBtn.setAttribute('id', 'search');
 
-let searchAppend = navbarEle.appendChild(searchBtn);
+const searchAppend = navbarEle.appendChild(searchBtn);
 
-let searchIcon = document.createElement('img');
+const searchIcon = document.createElement('img');
 searchIcon.setAttribute('src', './image/icon/search.png');
 searchAppend.appendChild(searchIcon);
 
 // ช่อง search
-let searchBar = document.createElement('span');
+const searchBar = document.createElement('span');
 searchBar.setAttribute('id', 'searchBar');
 navbarEle.appendChild(searchBar);
 
 // ปุ่ม clear
-let clearBtn = document.createElement('button');
+const clearBtn = document.createElement('button');
 clearBtn.setAttribute('id', 'clearBtn');
 clearBtn.textContent = 'Clear';
 navbarEle.appendChild(clearBtn);
 
 // เลขบนตะกร้า
-let amountInCart = document.createElement('span');
+const amountInCart = document.createElement('span');
 amountInCart.setAttribute('id', 'incart');
 navbarEle.appendChild(amountInCart);
 
 // รูป cart (ตะกร้า)
-let cartIcon = document.createElement('img');
+const cartIcon = document.createElement('img');
 cartIcon.setAttribute('id', 'cart');
 cartIcon.setAttribute('src', './image/icon/cart.png');
 navbarEle.appendChild(cartIcon);
 
 // สร้างปุ่มปรับโหมด
-let bgColor = document.querySelector("#bgColor");
+const bgColor = document.querySelector("#bgColor");
 
-let lightModeBtn = document.createElement("button");
+const lightModeBtn = document.createElement("button");
 lightModeBtn.setAttribute("id", "light");
 lightModeBtn.setAttribute("type", "button");
 bgColor.appendChild(lightModeBtn);
 
-let darkModeBtn = document.createElement("button");
+const darkModeBtn = document.createElement("button");
 darkModeBtn.setAttribute("id", "dark");
 darkModeBtn.setAttribute("type", "button");
 bgColor.appendChild(darkModeBtn);
 
-let appendIconLight = bgColor.appendChild(lightModeBtn);
+const appendIconLight = bgColor.appendChild(lightModeBtn);
 
-let iconLight = document.createElement("img");
+const iconLight = document.createElement("img");
 iconLight.setAttribute("class", "icon");
 iconLight.setAttribute("src", "./image/icon/sun.png");
 appendIconLight.appendChild(iconLight);
 
-let appendIconDark = bgColor.appendChild(darkModeBtn);
+const appendIconDark = bgColor.appendChild(darkModeBtn);
 
-let iconDark = document.createElement("img");
+const iconDark = document.createElement("img");
 iconDark.setAttribute("class", "icon");
 iconDark.setAttribute("src", "./image/icon/moon.png");
 appendIconDark.appendChild(iconDark);
@@ -66,43 +66,43 @@ appendIconDark.appendChild(iconDark);
 const productEle = document.querySelector('#products');
 for (let i = 0; i < product.length; i++) {
 
-    let createDiv = document.createElement('div');
+    const createDiv = document.createElement('div');
     createDiv.setAttribute('class', 'product');
     createDiv.setAttribute('id', product[i].productId);
 
-    let append = productEle.appendChild(createDiv);
+    const append = productEle.appendChild(createDiv);
 
-    let productName = document.createElement('h2');
+    const productName = document.createElement('h2');
     productName.setAttribute('class', product[i].productId);
     productName.textContent = product[i].productName;
 
-    let productType = document.createElement('p');
+    const productType = document.createElement('p');
     productType.textContent = product[i].productType;
 
-    let productSize = document.createElement('p');
+    const productSize = document.createElement('p');
     productSize.textContent = product[i].productSize;
 
-    let productDesc = document.createElement('p');
+    const productDesc = document.createElement('p');
     productDesc.textContent = product[i].productDesc;
 
-    let productPrice = document.createElement('p');
+    const productPrice = document.createElement('p');
     productPrice.textContent = 'price : ' + product[i].productPrice + ' BAHT';
 
-    let productImg = document.createElement('img');
+    const productImg = document.createElement('img');
     productImg.setAttribute('src', product[i].img);
 
-    let productStock = document.createElement('p');
+    const productStock = document.createElement('p');
     productStock.textContent = 'สินค้าคงเหลือ : ' + product[i].stock;
 
     // ปุ่ม add to cart
-    let addToCartBtn = document.createElement('button');
+    const addToCartBtn = document.createElement('button');
     addToCartBtn.setAttribute('class', 'add');
     addToCartBtn.setAttribute('type', 'button');
 
-    let appendBtn = append.appendChild(addToCartBtn);
+    const appendBtn = append.appendChild(addToCartBtn);
 
     // รูปปุ่ม add to cart
-    let addToCart = document.createElement('img');
+    const addToCart = document.createElement('img');
     addToCart.setAttribute('class', 'addImg');
     addToCart.setAttribute('src', './image/icon/addToCart.png');
     appendBtn.appendChild(addToCart);
